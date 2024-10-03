1bb398e1edef3c570dbc799e6be2ca44aa2b23fa
nicolas_alvarez <nicolasalvarezjunin@gmail.com>
2024-10-03 10:52:05 -0300

    .

---
+++ Curso_C++/Trabajos-Practicos/PI/Guia 2/Act_7.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int maxLibros = 500;
+
+struct Biblioteca
+{
+    string titulo;
+    string autor;
+    int ISBN;
+    string editorial;
+    int cantidadHojas;
+};
+
+void cargarLibros(Biblioteca libros[], int &dl)
+{
+    if (dl >= maxLibros) // Si el arreglo está lleno, no se puede cargar más libros
+    {
+        cout << "No hay más espacio para agregar libros." << endl;
+        return;
+    }
+    
+    for (int i = dl; i < maxLibros; i++)
+    {
+        cout << "Ingrese un titulo ('El hobbit' para finalizar): ";
+        getline(cin >> ws, libros[i].titulo); // Usar `ws` para limpiar espacios en blanco
+
+        if (libros[i].titulo == "El hobbit")
+        {
+            cout << "Libro 'El hobbit' detectado. Finalizando la carga..." << endl;
+            dl++; // Incrementa dl para contar también este libro
+            break;
+        }
+
+        cout << "\nIngrese el autor: ";
+        getline(cin, libros[i].autor);
+        cout << "Ingrese el ISBN: ";
+        cin >> libros[i].ISBN;
+        cin.ignore(); // Ignorar el salto de línea después de ingresar el número
+        cout << "Ingrese la editorial: ";
+        getline(cin, libros[i].editorial);
+        cout << "Ingrese la cantidad de hojas: ";
+        cin >> libros[i].cantidadHojas;
+        cin.ignore(); // Ignorar el salto de línea
+        cout << "----------" << endl;
+
+        dl++;
+
+        if (dl >= maxLibros)
+        {
+            cout << "Se alcanzó el máximo número de libros permitidos." << endl;
+            break;
+        }
+    }
+}
+
+void imprimirLibros(Biblioteca libros[], int dl) {
+
+    cout << "Libros cargados:" << endl;
+    for (int i = 0; i < dl; i++)
+    {
+        cout << "Libro " << i+1 << ":" << endl;
+        cout << "Titulo: " << libros[i].titulo << endl;
+        cout << "Autor: " << libros[i].autor << endl;
+        cout << "ISBN: " << libros[i].ISBN << endl;
+        cout << "Editorial: " << libros[i].editorial << endl;
+        cout << "Cantidad de hojas: " << libros[i].cantidadHojas << endl;
+        cout << "................." << endl;
+    }
+}
+
+void menu(Biblioteca libros[], int dl) {
+
+    char opcion;
+    do
+    {
+        cout << "\nBienvenido al sistema de la biblioteca" << endl;
+        cout << "1. Cargar libros" << endl;
+        cout << "2. Imprimir libros" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Elija una opcion: ";
+        cin >> opcion;
+
+        switch (opcion)
+        {
+        case 1:
+            cargarLibros(libros, dl);
+            break;
+        
+        case 2:
+            imprimirLibros(libros, dl);
+            break;
+
+        case 3:
+            cout << "Saliendo del programa..." << endl;
+            break;
+
+        default:
+            cout << "Ingrese una opcion valida." << endl;
+            break;
+        }
+
+    } while (opcion != '3');
+    
+}
+
+int main () {
+    int dl = 0;
+    Biblioteca libros[maxLibros];
+    menu(libros, dl);
+
+    return 0;
+}
---
+++ Curso_C++/Trabajos-Practicos/PI/Guia 2/Act_8.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int salonCapacidad = 150;
+
+struct Invitado
+{
+    string nombre;
+    string apellido;
+    char sexo;
+    int edad;
+};
+
+void cargarInvitados(Invitado invitados[], int& dl) {
+
+    if (dl >= salonCapacidad) // Si el arreglo está lleno, no se puede cargar más libros
+    {
+        cout << "\nNo hay más espacio para agregar invitados." << endl;
+        return;
+    }
+
+    string nombre;
+    cout << "\nIngrese el nombre del primer invitado('ZZZ' para salir de la carga): ";
+    getline(cin >> ws, nombre);
+
+    for (unsigned int i = 0; i < nombre.length(); i++)
+    {
+        nombre[i] = (toupper(nombre[i]));
+    }
+
+    while (nombre != "ZZZ" && dl < salonCapacidad)
+    {
+        invitados[dl].nombre = nombre;
+        cout << "Ingrese el apellido: ";
+        getline(cin >> ws, invitados[dl].apellido);
+        cout << "Ingrese el sexo(F o M): ";
+        cin >> invitados[dl].sexo;
+        cout << "Ingrese la edad: ";
+        cin >> invitados[dl].edad;
+        cout << "----------" << endl;
+
+        dl++; // Elemento cargado
+
+        cout << "\nIngrese el nombre del siguiente invitado('ZZZ' para salir de la carga): ";
+        getline(cin >> ws, nombre);
+
+        for (unsigned int i = 0; i < nombre.length(); i++)
+        {
+            nombre[i] = toupper(nombre[i]);
+        }
+    }
+}
+
+void imprimirInvitados(Invitado invitados[], int dl) {
+
+    cout << "\nInvitados a la fiesta:" << endl;
+    for (int i = 0; i < dl; i++)
+    {
+        cout << "Invitado " << i+1 << ":" << endl;
+        cout << "Nombre: " << invitados[i].nombre << endl;
+        cout << "Apellido: " << invitados[i].apellido << endl;
+        cout << "Sexo: " << invitados[i].sexo << endl;
+        cout << "Edad: " << invitados[i].edad << endl;
+        cout << "................." << endl;
+    }
+}
+
+void listaInvitados(Invitado invitados[], int dim) {
+
+    cout << "\nListado de mujeres menores de 20 años.";
+    for (int i = 0; i < dim; i++)
+    {
+        if (invitados[i].sexo == 'F' && invitados[i].edad <= 20)
+        {
+            cout << invitados[i].nombre;
+        }
+        cout << endl;
+    }
+}
+
+void eliminarInvitado(Invitado invitados[], int& dl) {
+
+    string Nombre, Apellido;
+    
+    cout << "\nIngrese el nombre y apellido a eliminar." << endl;
+    cout << "Nombre: ";
+    getline(cin >> ws, Nombre);
+    cout << "Apellido: ";
+    getline(cin >> ws, Apellido);
+    
+    int indice = 0;
+
+    while (indice <= dl && invitados[indice].nombre != Nombre || invitados[indice].apellido != Apellido)
+    {
+        indice++;
+    }
+
+    if (indice < dl)
+    {
+        for (int j = indice; j < dl-1; j++)
+        {
+            invitados[j].nombre = invitados[j+1].nombre;
+            invitados[j].apellido = invitados[j+1].apellido;
+            dl--;
+        }
+        cout << "Invitado eliminado.";
+    }
+    else
+    {
+        cout << "No se encontro el invitado.";
+    }
+}
+
+void menu(Invitado invitados[], int dl) {
+    char opcion;
+    do
+    {
+        cout << "\nBienvenido a la lista de invitados para su fiesta!." << endl;
+        cout << "1. Cargar invitados." << endl;
+        cout << "2. Imprimir invitados." << endl;
+        cout << "3. Lista de mujeres con menos de 20 anos." << endl;
+        cout << "4. Eliminar invitado." << endl;
+        cout << "5. Salir" << endl;
+        cout << "Elija una opcion: ";
+        cin >> opcion;
+
+        switch (opcion)
+        {
+        case '1':
+            cargarInvitados(invitados, dl);
+            break;
+
+        case '2':
+            imprimirInvitados(invitados, dl);
+            break;
+
+        case '3':
+            listaInvitados(invitados, dl);
+            break;
+
+        case '4':
+            eliminarInvitado(invitados, dl);
+            break;
+
+        case '5':
+            cout << "Saliendo del programa..." << endl;
+            break;
+        
+        default:
+            cout << "Ingrese una opcion valida." << endl;
+            break;
+        }
+    } while (opcion != '5');
+}
+
+int main () {
+    int dl = 0;
+    Invitado invitados[salonCapacidad]; 
+    menu(invitados, dl);
+
+    return 0;
+}
