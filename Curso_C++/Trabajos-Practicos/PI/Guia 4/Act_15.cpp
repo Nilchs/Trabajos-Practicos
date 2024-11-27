89e1dcdf14fd02d02034df2d88a781599d111b8e
nicolas_alvarez <nicolasalvarezjunin@gmail.com>
2024-11-27 19:05:07 -0300

    .

--- Curso_C++/Trabajos-Practicos/PI/Guia 4/Act_15.cpp
+++ Curso_C++/Trabajos-Practicos/PI/Guia 4/Act_15.cpp
@@ -7,7 +7,48 @@ de que la lista esté vacía, deberá retornar 0.
 Ejemplo: si la lista contiene 5 - 2 - 1 - 9 retornará 17.
 */
 
+struct Nodo
+{
+    int dato;
+    Nodo* siguiente;
+};
+
+Nodo* insertarAlInicio(Nodo* inicio, int numero)
+{
+    Nodo* nuevoNodo = new Nodo; // Crear nodo nuevo
+    nuevoNodo -> dato = numero;
+    nuevoNodo -> siguiente = inicio;
+    inicio = nuevoNodo; // Que el nuevo nodo sea el inicio, osea el primer elemento
+    return inicio;
+}
+
+int sumaLista(Nodo* inicio)
+{
+    if(inicio == nullptr) // Caso base: Si la lista está vacía
+    {
+        return 0;
+    }
+    else
+    {
+        return inicio -> dato + sumaLista(inicio -> siguiente); // Caso recursivo: Sumar el dato del nodo actual con el siguiente nodo llamando a la función cambiando de elemento
+    }
+}
+
 int main () {
-        
+    Nodo* inicio = nullptr;  
+
+    int numero;
+    cout << "Ingrese un numero (-1 para finalizar): ";
+    cin >> numero;
+    while(numero != -1)
+    {
+        inicio = insertarAlInicio(inicio, numero);
+        cout << "Ingrese otro numero (-1 para finalizar): ";
+        cin >> numero;
+    }
+
+    int resultado = sumaLista(inicio);
+    cout << "La suma de los elementos de la lista es: " << resultado;
+          
     return 0;
 }
--- Curso_C++/Trabajos-Practicos/PI/Guia 4/Act_16.cpp
+++ Curso_C++/Trabajos-Practicos/PI/Guia 4/Act_16.cpp
@@ -8,7 +8,64 @@ lista sea más corta que la otra (por ejemplo, la lista A tiene 10000 nodos y la
 continúe contabilizando la cantidad de nodos de la lista más larga.
 */
 
+struct Nodo
+{
+    int dato;
+    Nodo* siguiente;
+};
+
+Nodo* insertarAlInicio(Nodo* inicio, int numero)
+{
+    Nodo* nuevoNodo = new Nodo; // Crear nodo nuevo
+    nuevoNodo -> dato = numero;
+    nuevoNodo -> siguiente = inicio;
+    inicio = nuevoNodo; // Que el nuevo nodo sea el inicio, osea el primer elemento
+    return inicio;
+}
+
+bool compararLongitudes(Nodo* listaA, Nodo* listaB) {
+    if (listaA == nullptr && listaB == nullptr) { // Case base: Si ambas listas están vacías
+        return true;
+    }
+    if (listaA == nullptr || listaB == nullptr) { // Caso base: Si una lista está vacia y la otra no
+        return false;
+    }
+
+    return compararLongitudes(listaA->siguiente, listaB->siguiente); // Caso recursivo: Continuar comparando los siguientes nodos de ambas listas
+}
+
 int main () {
+    Nodo* listaA = nullptr;
+    Nodo* listaB = nullptr;
+    int numero;
+
+    cout << "Ingrese numeros para la primera lista (-1 para finalizar): ";
+    cin >> numero;
+    while (numero != -1)
+    {
+        listaA = insertarAlInicio(listaA, numero);
+        cout << "Ingrese otro numero para la primera lista (-1 para finalizar): ";
+        cin >> numero;
+    }
+
+    cout << "Ingrese numeros para la segunda lista (-1 para finalizar): ";
+    cin >> numero;
+    while (numero != -1)
+    {
+        listaB = insertarAlInicio(listaB, numero);
+        cout << "Ingrese otro numero para la segunda lista (-1 para finalizar): ";
+        cin >> numero;
+    }
+
+    if (!compararLongitudes(listaA, listaB))
+    {
+        
+        cout << "Las listas no tienen la misma longitud." << endl;
+    }
+    else
+    {
+        cout << "Las listas tienen la misma longitud." << endl;
+    }
         
     return 0;
 }
