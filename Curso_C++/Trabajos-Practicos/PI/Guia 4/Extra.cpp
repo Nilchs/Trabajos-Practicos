d35a048a5934483d832d2149474d92d2c2cef271
nicolas_alvarez <nicolasalvarezjunin@gmail.com>
2024-11-28 10:46:51 -0300

    .

---
+++ Curso_C++/Trabajos-Practicos/PI/Guia 4/Extra.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+int f_recursiva(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    else {
+        return n + f_recursiva(n - 1);
+    }
+}
+
+void print_reverse(string s) {
+
+    if (s.length() == 0) return;
+
+    print_reverse(s.substr(1));
+
+    cout << s[0];
+
+}
+
+int funcion1(int a, int b) {
+    if(a < b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+
+int funcion2(int arreglo[], int dl) {
+    if (dl == 1) {
+        return arreglo[0];
+    }
+    else {
+        return funcion1(arreglo[dl - 1], funcion2(arreglo, dl - 1));
+    }
+}
+
+void count_down(int n) {
+
+    if (n <= 0) {
+        cout << "Done!" << endl;
+
+    } else {
+        cout << n << endl;
+
+        count_down(n - 1);
+    }
+}
+
+int main() {
+    int a[] = {6,2,3,7,9,4};
+    int dl = 6;
+    cout << funcion2(a, dl) << " ";
+
+    print_reverse("abc");
+
+    return 0;
+}
+*/
