fbf6a9818d12ccb671980787e12918e0e0ce0c8b
nicolas_alvarez <nicolasalvarezjunin@gmail.com>
2025-04-17 11:09:18 -0300

    .

--- sample-app/Curso_GO/EJ Guia 2/EJ 2_e/Guia2_e.go
+++ sample-app/Curso_GO/EJ Guia 2/EJ 2_e/Guia2_e.go
@@ -10,8 +10,19 @@ func invertirIterativo(a []int) []int {
 	return aNew
 }
 
+func invertirRecursivo(i int, aNew, a []int) []int {
+	if i >= len(a) {
+		return aNew
+	}
+	aNew[i] = a[len(a)-1-i]
+	return invertirRecursivo(i+1, aNew, a)
+}
+
 func main() {
 	arreglo := []int{1, 2, 3}
+	arregloNew := make([]int, len(arreglo))
 	a1 := invertirIterativo(arreglo)
-	fmt.Printf("El arreglo invertido queda: %v\n", a1)
+	fmt.Printf("El arreglo invertido de forma iterativa queda: %v\n", a1)
+	a2 := invertirRecursivo(0, arregloNew, arreglo)
+	fmt.Printf("El arreglo invertido de forma recursiva queda: %v\n", a2)
 }
